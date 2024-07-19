c5ef0e14d1590fe85c9ed626728753471876c757
Cohnny Flobrandt <119353835+Cohnny@users.noreply.github.com>
2024-07-19 22:35:41 +0200

    Git Commit

---
+++ Source/Project0/Private/Project0Character.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Project0Character.h"
+
+// Sets default values
+AProject0Character::AProject0Character()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AProject0Character::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AProject0Character::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AProject0Character::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
