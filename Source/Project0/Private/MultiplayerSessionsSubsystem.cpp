801c0e41c53a1f51fdcb71ea90a5e970b3c5436d
Cohnny Flobrandt <119353835+Cohnny@users.noreply.github.com>
2024-07-30 01:11:55 +0200

    Git Commit

---
+++ Source/Project0/Private/MultiplayerSessionsSubsystem.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MultiplayerSessionsSubsystem.h"
+
+void PrintString(const FString& Str)
+{
+	if(GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Cyan, Str);
+	}
+}
+
+UMultiplayerSessionsSubsystem::UMultiplayerSessionsSubsystem()
+{
+	PrintString("MSS Constructor");
+}
+
+void UMultiplayerSessionsSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+
+	PrintString("MSS Initialize");
+}
+
+void UMultiplayerSessionsSubsystem::Deinitialize()
+{
+	Super::Deinitialize();
+
+	UE_LOG(LogTemp, Warning, TEXT("MSS Deinitialize"));
+}
